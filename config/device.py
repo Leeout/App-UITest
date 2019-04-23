2fd918880984fc077cfcdb74f9662a644e9c9e97
Lee <lee.lij@dadaabc.com>
2019-04-23 13:51:42 +0800

    update .gitignore

--- config/device.py
+++ config/device.py
@@ -6,8 +6,8 @@ device = {
     "platformName": "Android",
     "platformVersion": "6.0.1",
     "deviceName": "HuaWei",
-    "udid": "emulator-5554",  # 安卓模拟器唯一标识
-    "appActivity": ".account.accountlogin.LoginActivity",  # 登录页Activity
+    "udid": "emulator-5554",
+    "appActivity": ".account.accountlogin.LoginActivity",
     "appPackage": "com.dadaabc.zhuozan.dadaabcstudent",
     "remoteUrl": "http://localhost:4723/wd/hub"
 }
@@ -17,9 +17,9 @@ def device_info():
     desired_caps = dict()
     desired_caps['platformName'] = device['platformName']  # 设置平台       
     desired_caps['platformVersion'] = device['platformVersion']  # 系统版本       
-    desired_caps['deviceName'] = device['deviceName']  # 设备id       
-    desired_caps['appPackage'] = device['appPackage']  # 包名      
-    desired_caps['appActivity'] = device['appActivity']  # 启动的Activity 
-    my_driver = webdriver.Remote(device['remoteUrl'], desired_caps)
+    desired_caps['deviceName'] = device['deviceName']  # 设备id 安卓机唯一标识   
+    desired_caps['appPackage'] = device['appPackage']  # app包名      
+    desired_caps['appActivity'] = device['appActivity']  # 启动的activity 
+    my_driver = webdriver.Remote(device['remoteUrl'], desired_caps)  # 接收指令的appium server端
     logger.info('设备信息：%s', desired_caps)
     return my_driver
