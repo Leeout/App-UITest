2170436df992aed982859a6531c0b30bc3b6feb3
李柯 <jason.lik@likedeMacBook-Pro.local>
2019-07-29 18:08:33 +0800

    run.sh去除冗余代码

--- run.sh
+++ run.sh
@@ -1,40 +1,24 @@
 #!/usr/bin/env bash
-#启动说明
-#参数$1:指定被测设备类型 如：ipad || iphone || android
+#启动说明（仅支持ios设备）
+#参数$1:指定被测设备类型 如：ipad（学生端）|| iphone（家长端）
 #参数$2:指定运行测试用例的次数 如：3
 
-get_WebDriverAgentRunner(){
-bundle_id=$(ideviceinstaller -l | grep WebDriverAgentRunner-Runner | awk 'BEGIN{ RS=","; } { print $0 }' | head -n1)
-if [[ ${bundle_id} == "com.apple.test.WebDriverAgentRunner-Runner" ]]; then
-    return 0
-else
-    return 1
-fi
-}
-
-init_device(){
+handle(){
 # shellcheck disable=SC2078
 if [[ ${1} == "ipad" || "iphone" ]]; then
-    func=$(get_WebDriverAgentRunner)
-    if [[ ${func} -ne 0 ]]; then
-        password="1234"
-        security unlock-keychain -p $password ~/Library/Keychains/login.keychain
-        udid=$(idevice_id -l | head -n1)
-        echo "开始build WebDriverAgent......"
-        xcodebuild -project /Users/jason.lik/WebDriverAgent/WebDriverAgent.xcodeproj  -scheme WebDriverAgentRunner -destination "id=$udid" test
-        echo "build WebDriverAgent完成"
-        return $?
-    else
-        return 0
+    udid=$(idevice_id -l | head -n1)
+    bundle_id=$(ideviceinstaller -l | grep WebDriverAgentRunner-Runner | awk 'BEGIN{ RS=","; } { print $0 }' | head -n1)
+    if [[ ! (${udid} && ${bundle_id}) ]]; then
+      echo "测试设备初始化失败,请检查设备连接或WebDriverAgentRunner是否正常安装！"
+      exit 1
     fi
 else
-    echo "开始启动android adb服务......"
-    adb start-server
-    return $?
+    echo "设备参数有误，请重新输入！"
+    exit 1
 fi
 }
 
-if init_device "$1";then
+if handle "$1";then
     num=1
     while [[ ${num} -le $2 ]]
     do
@@ -42,7 +26,4 @@ if init_device "$1";then
         python3 command_run.py -r "$1"
         (( num++ ))
     done
-else
-    echo "初始化设备环境失败，请手动build WebDriverAgentRunner!"
-    exit 1
 fi
